a881355f9a2c2855cf3121f25f5f07c9272392dd
Muyale Okoyo <annmuyale@gmail.com>
2023-05-25 20:24:36 +0300

    first commit

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ document.body.style.backgroundColor='silver'
 document.getElementById('title').style.color='green'
 const h3Elements = document.querySelectorAll('h3');
 h3Elements.forEach((h3) => {
-  h3.style.textTransform = 'uppercase';
+h3.style.textTransform = 'uppercase';
+
 });
+var fruits = document.getElementById('fruList')
+var newFruit = document.createElement('li')
+newFruit.textContent = 'Banana';
+fruits.appendChild(newFruit)
+var vegies = document.getElementById('vegList')
+var newVegie = document.createElement('li');
+newVegie.textContent = 'Apple';
+vegies.appendChild(newVegie)
+
+
+
+
+
